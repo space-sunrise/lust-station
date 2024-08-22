200b9f4f9970e8df15c9d30c3612206773db29d8
VigersRay <vigersray@gmail.com>
2024-08-22 12:11:37 +0300

    UPD CDN URL

--- Tools/publish_github_artifact.py
+++ Tools/publish_github_artifact.py
@@ -15,7 +15,7 @@ VERSION = os.environ['GITHUB_SHA']
 # Forks should change these to publish to their own infrastructure.
 #
 ROBUST_CDN_URL = "https://ss14.cdn.shizainc.com/"
-FORK_ID = "sunrise_station"
+FORK_ID = "lust_station"
 
 def main():
     print("Fetching artifact URL from API...")
