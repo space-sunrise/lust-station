3a3a63225801d27b850ca89702d9e259dfdd9a1e
drdth <drdtheuser@gmail.com>
2024-09-03 21:27:44 +0300

    add resting system

---
+++ Content.Shared/_Lust/Rest/RestAbilityStuff.cs
@@ -0,0 +1,53 @@
+﻿using Content.Shared.Actions;
+using Content.Shared.DoAfter;
+using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._Lust.Rest;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class RestAbilityComponent : Component
+{
+    [DataField] public bool IsResting;
+
+    [DataField] public TimeSpan Cooldown = TimeSpan.FromSeconds(1);
+
+    /// <summary>
+    /// Бекап скорости передвижения, так как она сбрасывается до нуля при сидении
+    /// </summary>
+    public float PreviousWalkSpeed;
+
+    /// <summary>
+    /// Бекап скорости передвижения, так как она сбрасывается до нуля при сидении
+    /// </summary>
+    public float PreviousSprintSpeed;
+
+    /// <summary>
+    /// Список слоев, которые будут выключаться вместе с основным спрайтом. В строках.
+    /// </summary>
+    [DataField]
+    public HashSet<string> DisableableStringLayers= new ();
+
+    /// <summary>
+    /// Список слоев, которые будут выключаться вместе с основным спрайтом. В енумах
+    /// </summary>
+    [DataField]
+    public HashSet<Enum> DisableableEnumLayers= new ();
+}
+
+[Serializable, NetSerializable]
+public enum RestVisuals : byte
+{
+    Base,
+    Resting,
+}
+
+public sealed partial class RestActionEvent : InstantActionEvent {}
+
+[Serializable, NetSerializable]
+public sealed partial class RestDoAfterEvent : SimpleDoAfterEvent {}
+
+public sealed partial class RestChangeSpriteEvent : EntityEventArgs
+{
+    public EntityUid Entity;
+}
---
+++ Content.Shared/_Lust/Rest/SharedRestSystem.cs
@@ -0,0 +1,82 @@
+﻿using Content.Shared.DoAfter;
+using Content.Shared.Movement.Components;
+using Content.Shared.Movement.Systems;
+using Robust.Shared.Timing;
+
+namespace Content.Shared._Lust.Rest;
+
+public sealed class SharedRestSystem : EntitySystem
+{
+    [Dependency] private readonly SharedDoAfterSystem _doAfter = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly MovementSpeedModifierSystem _movementSpeedModifier = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<RestAbilityComponent, RestActionEvent>(OnActionToggled);
+        SubscribeLocalEvent<RestAbilityComponent, RestDoAfterEvent>(OnSuccess);
+    }
+
+    private void OnActionToggled(EntityUid uid, RestAbilityComponent ability, RestActionEvent args)
+    {
+        var doAfterEventArgs = new DoAfterArgs(EntityManager, uid, ability.Cooldown, new RestDoAfterEvent(), uid)
+        {
+            BreakOnMove = true,
+            BreakOnWeightlessMove = false,
+            BreakOnDamage = true,
+        };
+
+        _doAfter.TryStartDoAfter(doAfterEventArgs);
+    }
+
+    private void OnSuccess(EntityUid uid, RestAbilityComponent ability, RestDoAfterEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        if (args.Cancelled)
+            return;
+
+        if (!_timing.IsFirstTimePredicted)
+            return;
+
+        ability.IsResting = !ability.IsResting;
+        Dirty(uid, ability);
+
+        RaiseLocalEvent(uid, new RestChangeSpriteEvent{Entity = uid});
+        ToggleRestLogic(uid, ability);
+
+        args.Handled = true;
+    }
+
+    /// <summary>
+    /// !!SHITCODE ALERT!! Запрещает двигаться, посредством сбрасывания скорости в ноль, пока цель сидит
+    /// </summary>
+    /// <param name="uid">Цель</param>
+    /// <param name="ability">Компонент сидения, в нем хранится бекап данных</param>
+    private void ToggleRestLogic(EntityUid uid, RestAbilityComponent ability)
+    {
+        if (!TryComp<MovementSpeedModifierComponent>(uid, out var movementSpeed))
+            return;
+
+        var walkSpeed = 0f;
+        var sprintSpeed = 0f;
+
+        if (ability.IsResting)
+        {
+            ability.PreviousWalkSpeed = movementSpeed.BaseWalkSpeed;
+            ability.PreviousSprintSpeed = movementSpeed.BaseSprintSpeed;
+
+            Dirty(uid, ability);
+        }
+        else
+        {
+            walkSpeed = ability.PreviousWalkSpeed;
+            sprintSpeed = ability.PreviousSprintSpeed;
+        }
+
+        _movementSpeedModifier.ChangeBaseSpeed(uid, walkSpeed, sprintSpeed, movementSpeed.Acceleration);
+    }
+}
