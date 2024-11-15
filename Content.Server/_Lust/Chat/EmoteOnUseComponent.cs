d4461c609083ac9f0e36d4a9ea37301e489ca8f3
pacable <77161122+pxc1984@users.noreply.github.com>
2024-11-15 22:13:50 +0300

    2 эксклюзивные для ласта плюшки (#71)
    * анаме германикум
    
    * ERP плюшка анны лисечанской
    
    * лови аптечку
    
    * стон
    
    * заставил ее эмоутить

---
+++ Content.Server/_Lust/Chat/EmoteOnUseComponent.cs
@@ -0,0 +1,8 @@
+﻿namespace Content.Server._Lust.Chat;
+
+[RegisterComponent]
+public sealed partial class EmoteOnUseComponent : Component
+{
+    [DataField]
+    public List<string> Values;
+}
---
+++ Content.Server/_Lust/Chat/EmoteOnUseSystem.cs
@@ -0,0 +1,34 @@
+﻿using Content.Server.Chat.Systems;
+using Content.Shared.Interaction.Events;
+using Content.Shared.Timing;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Random;
+
+namespace Content.Server._Lust.Chat;
+
+public sealed class EmoteOnUseSystem : EntitySystem
+{
+    [Dependency] private readonly UseDelaySystem _useDelay = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] private readonly ChatSystem _chat = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<EmoteOnUseComponent, UseInHandEvent>(OnUseInHand);
+    }
+
+    public void OnUseInHand(EntityUid uid, EmoteOnUseComponent? component, UseInHandEvent args)
+    {
+        if (!Resolve(uid, ref component))
+            return;
+
+        if (!TryComp<UseDelayComponent>(uid, out var useDelayComponent) || _useDelay.IsDelayed((uid, useDelayComponent)))
+            return;
+
+        _chat.TryEmoteWithChat(uid, _random.Pick(component.Values));
+        _useDelay.TryResetDelay((uid, useDelayComponent));
+    }
+}
