05886353e4731d8b42622eea2d3af93ece1aec19
pacable <igor.mamaev1@gmail.com>
2024-11-19 21:14:03 +0300

    Initial commit

---
+++ Content.Server/_Lust/Traits/Components/HighPainThresholdTraitComponent.cs
@@ -0,0 +1,10 @@
+﻿using Content.Shared.FixedPoint;
+
+namespace Content.Server._Lust.Traits.Components;
+
+[RegisterComponent]
+public sealed partial class HighPainThresholdTraitComponent : Component
+{
+    [DataField]
+    public FixedPoint2 Decrease = 25;
+}
---
+++ Content.Server/_Lust/Traits/Components/ProzopagosiaTraitComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace Content.Server._Lust.Traits.Components;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class ProzopagosiaTraitComponent : Component
+{
+    
+}
