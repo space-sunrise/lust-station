bf2d64015908e7ceb704dbee7742d34b6bd64cf0
pacable <igor.mamaev1@gmail.com>
2024-11-19 21:14:12 +0300

    HighPainThresholdSystem.cs

---
+++ Content.Server/_Lust/Traits/Systems/HighPainThresholdSystem.cs
@@ -0,0 +1,24 @@
+﻿using Content.Server._Lust.Traits.Components;
+using Content.Shared.Mobs;
+using Content.Shared.Mobs.Components;
+using Content.Shared.Mobs.Systems;
+
+namespace Content.Server._Lust.Traits.Systems;
+
+public sealed class HighPainThresholdSystem : EntitySystem
+{
+    [Dependency] private readonly MobThresholdSystem _threshold = default!;
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<HighPainThresholdTraitComponent, ComponentInit>(OnInit);
+    }
+
+    private void OnInit(EntityUid uid, HighPainThresholdTraitComponent component, ComponentInit args)
+    {
+        if (!TryComp(uid, out MobThresholdsComponent? thresholdsComponent))
+            return;
+        var critDmg = _threshold.GetThresholdForState(uid, MobState.Critical, thresholdsComponent);
+        _threshold.SetMobStateThreshold(uid, critDmg - component.Decrease, MobState.Critical, thresholdsComponent);
+    }
+}
