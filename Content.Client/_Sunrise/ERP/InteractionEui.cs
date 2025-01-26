e3758196d352ebd409e14dd74d85f7fe8733cb45
iertis <kanopus952@gmail.com>
2025-01-26 15:00:06 +0300

    Reapply "Доработка панели взаимодействия (#145)"
    This reverts commit 2bc550cadc34556c1c89a95bba70c05008d52418.

--- Content.Client/_Sunrise/ERP/InteractionEui.cs
+++ Content.Client/_Sunrise/ERP/InteractionEui.cs
@@ -10,15 +10,15 @@ using Content.Client.Chat.Managers;
 using Robust.Client.Player;
 using Robust.Shared.Timing;
 using Content.Shared.IdentityManagement;
+
 namespace Content.Client._Sunrise.ERP
 {
     [UsedImplicitly]
     public sealed class InteractionEui : BaseEui
     {
-        private readonly InteractionWindow _window;
+        private InteractionWindow _window;
         public IEntityManager _entManager;
 
-        [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
         [Dependency] private readonly IRobustRandom _random = default!;
         [Dependency] private readonly IChatManager _chat = default!;
         [Dependency] private readonly IPlayerManager _player = default!;
@@ -27,7 +27,10 @@ namespace Content.Client._Sunrise.ERP
         {
             _entManager = IoCManager.Resolve<IEntityManager>();
             _window = new InteractionWindow(this);
-            _window.OnClose += OnClosed;
+            _window.OnClose += () =>
+            {
+                SendMessage(new CloseEuiMessage());
+            };
         }
 
         public override void HandleMessage(EuiMessageBase msg)
@@ -67,13 +70,9 @@ namespace Content.Client._Sunrise.ERP
             SendMessage(new RequestInteractionState());
         }
 
-        private void OnClosed()
-        {
-            SendMessage(new CloseEuiMessage());
-        }
-
         public override void Opened()
         {
+            base.Opened();
             _window.OpenCenteredLeft();
         }
 
@@ -102,10 +101,10 @@ namespace Content.Client._Sunrise.ERP
 
         public void FrameUpdate(FrameEventArgs args)
         {
-            foreach((var item, var time, var text) in _disabledItems)
+            foreach ((var item, var time, var text) in _disabledItems)
             {
                 item.Text = text + $" ({(time - _gameTiming.CurTime).Seconds} сек.)";
-                if(_gameTiming.CurTime >= time)
+                if (_gameTiming.CurTime >= time)
                 {
                     item.Text = text;
                     item.Disabled = false;
