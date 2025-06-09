eb6e23b48dfeac1d7b18e44e2f754af90dd1c138
Vigers Ray <vigersray@gmail.com>
2025-06-09 21:23:52 +0300

    заменил стандартные значения с прода по дефолту

--- Content.Shared/CCVar/CCVars.Shuttle.cs
+++ Content.Shared/CCVar/CCVars.Shuttle.cs
@@ -141,14 +141,14 @@ public sealed partial class CCVars
     ///     How long after the console is authorized for the shuttle to early launch.
     /// </summary>
     public static readonly CVarDef<float> EmergencyShuttleAuthorizeTime =
-        CVarDef.Create("shuttle.emergency_authorize_time", 10f, CVar.SERVERONLY);
+        CVarDef.Create("shuttle.emergency_authorize_time", 20f, CVar.SERVERONLY);
 
     /// <summary>
     ///     The minimum time for the emergency shuttle to arrive at centcomm.
     ///     Actual minimum travel time cannot be less than <see cref="ShuttleSystem.DefaultArrivalTime"/>
     /// </summary>
     public static readonly CVarDef<float> EmergencyShuttleMinTransitTime =
-        CVarDef.Create("shuttle.emergency_transit_time_min", 60f, CVar.SERVERONLY);
+        CVarDef.Create("shuttle.emergency_transit_time_min", 90f, CVar.SERVERONLY);
 
     /// <summary>
     ///     The maximum time for the emergency shuttle to arrive at centcomm.
