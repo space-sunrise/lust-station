0d54600c284b88898790a26643f2459a30c06536
iertis <kanopus952@gmail.com>
2025-07-13 22:21:47 +0500

    a

--- Content.Shared/_Lust/Clothing/EmitSoundOnErpSystem.cs
+++ Content.Shared/_Lust/Clothing/EmitSoundOnErpSystem.cs
@@ -1,26 +1,25 @@
-﻿using Content.Shared._Sunrise.ERP;
-using Robust.Shared.Audio.Systems;
-using Robust.Shared.Timing;
+﻿// using Robust.Shared.Audio.Systems;
+// using Robust.Shared.Timing;
 
-namespace Content.Shared._Lust.Clothing;
+// namespace Content.Shared._Lust.Clothing;
 
-public sealed class EmitSoundOnErpSystem : EntitySystem
-{
-    [Dependency] private readonly IGameTiming _timing = default!;
-    [Dependency] private readonly SharedAudioSystem _audio = default!;
+// public sealed class EmitSoundOnErpSystem : EntitySystem
+// {
+//     [Dependency] private readonly IGameTiming _timing = default!;
+//     [Dependency] private readonly SharedAudioSystem _audio = default!;
 
-    /// <inheritdoc/>
-    public override void Initialize()
-    {
-        SubscribeLocalEvent<_Lust.Clothing.EmitSoundOnErpComponent, ClothingErpOccuredEvent>(OnErp);
-    }
+//     /// <inheritdoc/>
+//     public override void Initialize()
+//     {
+//         SubscribeLocalEvent<_Lust.Clothing.EmitSoundOnErpComponent, ClothingErpOccuredEvent>(OnErp);
+//     }
 
-    private void OnErp(EntityUid uid, _Lust.Clothing.EmitSoundOnErpComponent component, ClothingErpOccuredEvent args)
-    {
-        if (_timing.CurTime - component.PrevSound < component.Cooldown)
-            return;
-        component.PrevSound = _timing.CurTime;
+//     private void OnErp(EntityUid uid, _Lust.Clothing.EmitSoundOnErpComponent component, ClothingErpOccuredEvent args)
+//     {
+//         if (_timing.CurTime - component.PrevSound < component.Cooldown)
+//             return;
+//         component.PrevSound = _timing.CurTime;
 
-        _audio.PlayPvs(component.Sound, uid);
-    }
-}
+//         _audio.PlayPvs(component.Sound, uid);
+//     }
+// }
