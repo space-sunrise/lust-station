050004f70d8b7af0d47a0f5f4d49dfc198304f81
Vigers Ray <vigersray@gmail.com>
2025-08-06 20:49:17 +0300

    новый cdn
    (cherry picked from commit 9aaed62bb32a77e08327bc9143c14cc4744d256d)

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -15,8 +15,8 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://ss14.cdn.shizainc.com/" # Sunrise-Edit
-FORK_ID = "lust_station" # Lust-Edit
+ROBUST_CDN_URL = "https://cdn.ss14.org/" # Sunrise-Edit
+FORK_ID = "sunrise_station" # Sunrise-Edit
 
 def main():
     parser = argparse.ArgumentParser()
