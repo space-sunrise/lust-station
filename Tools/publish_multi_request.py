yarp0d/fish-station

Update publish_multi_request.py

Tools/publish_multi_request.py
@@ -16,7 +16,7 @@ RELEASE_DIR = "release"
 # Forks should change these to publish to their own infrastructure.
 #
 ROBUST_CDN_URL = "https://ss14.cdn.shizainc.com/" # Sunrise-Edit
-FORK_ID = "sunrise_station" # Sunrise-Edit
+FORK_ID = "fish_station" # Sunrise-Edit
 
 def main():
     parser = argparse.ArgumentParser()

space-sunrise/lust-station

Tools/publish_multi_request.py
@@ -16,7 +16,7 @@ RELEASE_DIR = "release"
 # Forks should change these to publish to their own infrastructure.
 #
 ROBUST_CDN_URL = "https://ss14.cdn.shizainc.com/" # Sunrise-Edit
-FORK_ID = "lust_station" # Lust-Edit
+FORK_ID = "paradise_station" # Sunrise-Edit
 
 def main():
     parser = argparse.ArgumentParser()