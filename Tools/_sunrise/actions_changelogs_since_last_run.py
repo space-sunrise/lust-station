b6c76eb229105d9a87d19a8ce17122dacc5b05da
Vigers Ray <vigersray@gmail.com>
2025-08-07 17:08:47 +0300

    пинг ждунов
    (cherry picked from commit 1a2baf92653a90b5343004778b49b5b6341bd2ce)

--- Tools/_sunrise/actions_changelogs_since_last_run.py
+++ Tools/_sunrise/actions_changelogs_since_last_run.py
@@ -22,7 +22,7 @@ GITHUB_API_URL    = os.environ.get("GITHUB_API_URL", "https://api.github.com")
 DISCORD_SPLIT_LIMIT = 2000
 DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL")
 
-CHANGELOG_FILE = "Resources/Changelog/ChangelogLust.yml"
+CHANGELOG_FILE = os.environ.get("CHANGELOG_FILE", "Resources/Changelog/ChangelogSunrise.yml")
 
 TYPES_TO_EMOJI = {
     "Fix":    "🪛",
