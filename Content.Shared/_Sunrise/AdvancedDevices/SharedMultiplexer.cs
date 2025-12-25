0ce788962ab3a4ee64492d67d03935c94ea25e60
Txor <136470242+timoxxor@users.noreply.github.com>
2025-12-25 20:38:49 +0300

    Добавлены mux/demux (#3424)
    Co-authored-by: timoxxor <timoxxor@duck.com>
    Co-authored-by: iertis <kanopus952@gmail.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ Content.Shared/_Sunrise/AdvancedDevices/SharedMultiplexer.cs
@@ -0,0 +1,22 @@
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._Sunrise.AdvancedDevices;
+
+[Serializable, NetSerializable]
+public enum MultiplexerVisuals : byte
+{
+    Gate
+}
+
+[Serializable, NetSerializable]
+public enum MultiplexerLayers : byte
+{
+    Gate
+}
+
+[Serializable, NetSerializable]
+public enum MuxState : byte
+{
+    Demux,
+    Mux
+}
