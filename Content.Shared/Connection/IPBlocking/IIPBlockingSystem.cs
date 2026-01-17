ef24107a24470ae8db11e99601152cd3aa29c2fa
Vigers Ray <vigersray@gmail.com>
2026-01-17 23:39:24 +0100

    Добавлены новые методы для блокировки IP-адресов с указанием причины и времени действия.

--- Content.Shared/Connection/IPBlocking/IIPBlockingSystem.cs
+++ Content.Shared/Connection/IPBlocking/IIPBlockingSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net;
 
 namespace Content.Shared.Connection.IPBlocking;
@@ -7,6 +8,10 @@ public interface IIPBlockingSystem
     void Initialize();
 
     bool IsBlocked(IPAddress ip);
+
+    void BlockIP(IPAddress ip, TimeSpan duration, string reason);
+
+    void BlockIP(IPAddress ip, string reason);
 
     bool CheckAndBlockSuspiciousLength(IPAddress ip, int length, string context);
 
